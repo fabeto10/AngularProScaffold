45d146dd7abd2c67bf254990f0f54deaf6ce69af
Isaac <itzhak.yohananov@gmail.com>
2023-03-17 22:21:38 +0200

    fix login in auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
 import { TokenService } from './token.service';
 import { Store } from '@ngrx/store';
 import { login, logout } from '../../store/auth/auth.actions';
@@ -10,20 +8,16 @@ import { login, logout } from '../../store/auth/auth.actions';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'https://your-auth-api-url';
+  private apiUrl = 'https://your-auth-api-url'; // Replace with your authentication API URL
+
   constructor(
     private http: HttpClient,
     private tokenService: TokenService,
     private store: Store
   ) {}
 
-  login(email: string, password: string): Observable<{ token: string }> {
-    const payload = {
-      email,
-      password,
-    };
-
-    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, payload);
+  login(email: string, password: string): void {
+    this.store.dispatch(login({ email, password }));
   }
 
   logout(): void {
--- src/app/store/auth/auth.effects.ts
+++ src/app/store/auth/auth.effects.ts
@@ -2,20 +2,31 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, exhaustMap, map, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 import { AuthService } from '../../core/services/auth.service';
 import { login, loginSuccess, loginFailure, logout } from './auth.actions';
 
 @Injectable()
 export class AuthEffects {
+  private apiUrl = 'https://your-auth-api-url';
+
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private http: HttpClient
+  ) {}
+
   login$ = createEffect(() =>
     this.actions$.pipe(
       ofType(login),
       exhaustMap(({ email, password }) =>
-        this.authService.login(email, password).pipe(
-          map((response) => loginSuccess({ token: response.token })),
-          catchError((error) => of(loginFailure({ error })))
-        )
+        this.http
+          .post<{ token: string }>(`${this.apiUrl}/login`, { email, password })
+          .pipe(
+            map((response) => loginSuccess({ token: response.token })),
+            catchError((error) => of(loginFailure({ error })))
+          )
       )
     )
   );
@@ -28,6 +39,4 @@ export class AuthEffects {
       ),
     { dispatch: false }
   );
-
-  constructor(private actions$: Actions, private authService: AuthService) {}
 }
